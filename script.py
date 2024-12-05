f6bb13dedeb9690c10dc85cfc953a05c98ad161f
Zana Aziz <mail@zanaaziz.com>
2024-12-05 13:16:23 +0000

    added collection of geolocation

--- script.py
+++ script.py
@@ -6,11 +6,50 @@ import time
 from sense_hat import SenseHat
 
 sense = SenseHat()
-backend_api_url = "https://iot-raspberrypi-backend-12a94ca200cf.herokuapp.com"
+
+def get_ip_geolocation():
+    """
+    sends a HTTP GET request to a free 3rd party API which returns geolocation data based on this device's IP address
+    """
+
+    # create request object
+    req = urllib.request.Request("https://ipapi.co/json", method="GET")
+
+    try:
+        # send request and handle response
+        with urllib.request.urlopen(req) as response:
+            response_data = response.read().decode("utf-8")
+            json_data = json.loads(response_data)
+            return json_data
+
+    except urllib.error.HTTPError as e:
+        print(f"HTTP error occurred: {e.code} {e.reason}")
+        return None
+
+    except urllib.error.URLError as e:
+        print(f"URL error occurred: {e.reason}")
+        return None
+
+# collecting geolocation data so it's readily available
+geolocation = get_ip_geolocation()
+latitude = geolocation["latitude"]
+longitude = geolocation["longitude"]
 
 # fetches current user from os
 def get_user_identifier():
     return pwd.getpwuid(os.getuid())[0]
+
+# gets humidity reading from sensor
+def get_humidity():
+    return sense.get_humidity()
+
+# gets pressure reading from sensor
+def get_pressure():
+    return sense.get_pressure()
+
+# gets temperature reading from sensor
+def get_temperature():
+    return sense.get_temperature()
 
 def send_post_request(endpoint, payload):
     """
@@ -23,11 +62,14 @@ def send_post_request(endpoint, payload):
 
     headers = {"Content-Type": "application/json"}
 
+    # url of backend API
+    base_url = "https://iot-raspberrypi-backend-12a94ca200cf.herokuapp.com"
+
     # convert payload to JSON and encode to bytes
     payload_json = json.dumps(payload).encode("utf-8")
 
     # create request object
-    req = urllib.request.Request(backend_api_url + endpoint, data=payload_json, headers=headers, method="POST")
+    req = urllib.request.Request(base_url + endpoint, data=payload_json, headers=headers, method="POST")
 
     try:
         # send request and handle response
@@ -47,7 +89,9 @@ def send_post_request(endpoint, payload):
 def submit_humidity():
     payload = {
         "deviceId": get_user_identifier(),
-        "value": str(sense.get_humidity())
+        "value": str(get_humidity()),
+        "latitude": str(latitude),
+        "longitude": str(longitude)
     }
 
     response_data, status_code = send_post_request("/humidities", payload)
@@ -62,7 +106,9 @@ def submit_humidity():
 def submit_pressure():
     payload = {
         "deviceId": get_user_identifier(),
-        "value": str(sense.get_pressure())
+        "value": str(get_pressure()),
+        "latitude": str(latitude),
+        "longitude": str(longitude)
     }
 
     response_data, status_code = send_post_request("/pressures", payload)
@@ -77,7 +123,9 @@ def submit_pressure():
 def submit_temperature():
     payload = {
         "deviceId": get_user_identifier(),
-        "value": str(sense.get_temperature())
+        "value": str(get_temperature()),
+        "latitude": str(latitude),
+        "longitude": str(longitude)
     }
 
     response_data, status_code = send_post_request("/temperatures", payload)
